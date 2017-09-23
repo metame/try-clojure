78546cdefea7d901bd1d45ff118adc134c0fb6e9
Michael Erwin <michaeljameserwin@gmail.com>
2017-09-23 01:00:26 -0600

    reduce

--- shire-model/src/shire_model/core.clj
+++ shire-model/src/shire_model/core.clj
@@ -29,13 +29,18 @@
 (defn symmetrize-body-parts
   "Expects a seq of maps that have a :name and :size"
   [asym-body-parts]
-  (loop [remaining-asym-parts asym-body-parts final-body-parts []]
-  (if (empty? remaining-asym-parts)
-    final-body-parts
-    (let [[part & remaining] remaining-asym-parts]
-      (recur remaining
-        (into final-body-parts
-          (set [part (matching-part part)])))))))
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)]))) 
+          [] 
+          asym-body-parts))
+
+  ; (loop [remaining-asym-parts asym-body-parts final-body-parts []]
+  ; (if (empty? remaining-asym-parts)
+  ;   final-body-parts
+  ;   (let [[part & remaining] remaining-asym-parts]
+  ;     (recur remaining
+  ;       (into final-body-parts
+  ;         (set [part (matching-part part)])))))))
 
 ; (symmetrize-body-parts asym-hobbit-body-parts)
 
